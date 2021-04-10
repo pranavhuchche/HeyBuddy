62875d99331dfbbabf28f9ee593a980c9178d1c6
Jamil Ahmad <jmlahmdpp225@gmail.com>
2021-04-10 15:03:25 +0530

    Create New Product API
    Create New Product API

---
+++ BuddyAppBackend/src/main/java/com/cs/hackathon/heybuddy/products/CreateProductController.java
@@ -0,0 +1,17 @@
+package com.cs.hackathon.heybuddy.products;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CreateProductController {
+	@Autowired
+	protected CreateProductService createProductService;
+
+	@PostMapping("/createproduct")
+	public Product getProductById(@RequestBody Product product) throws Exception {
+		return createProductService.createNewProduct(product);
+	}
+}
